0a4fd40f4c05c15d7def0b4e80e903bd50c54b48
Srushti <srushtideshmukh54@gmail.com>
2024-06-12 13:09:27 +0530

    new changes added

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,11 @@
 import classes from '../styles/projects.module.scss'
 
 const projectInfo = [
+    
     {
-        name: "Apple Clone",
-        description: "The website has been developed using the React framework and serves as a replica of a well-known Apple website in India.",
-        key: 1
+        name: "Intellirecruit",
+        description: "IntelliRecruit, built with React, optimizes resumes by providing feedback and compatibility scores based on job descriptions.",
+        key : 1
     },
     {
         name: "Codepen Clone",
@@ -15,16 +16,16 @@ const projectInfo = [
         name: "ListNest",
         description: "A React-based task management website that lets users add and remove tasks easily, keeping track of their to-do list.",
         key: 3
-    },
+    },   
     {
-        name: "DineEase",
-        description: "A Bootstrap-designed website that enables customers to make table reservations and place food orders online., CSS, and JS.",
+        name: "Apple Clone",
+        description: "The website has been developed using the React framework and serves as a replica of a well-known Apple website in India.",
         key: 4
     },
     {
-        name: "RA Prediction",
-        description: "A model using OpenCV, machine learning, and deep learning has been developed to predict the presence of Rheumatoid Arthritis by analyzing images.",
-        key : 5
+        name: "DrowsyGuard",
+        description: "A Drowsiness Detection System employing transfer learning to identify when drivers are drowsy, alerting them to prevent road accidents.",
+        key: 5
     },
 ]
 
