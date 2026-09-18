97724147031de0084d8a1d5ebb2391499e25fd52
Srushti2002 <srushtideshmukh54@gmail.com>
2026-09-18 21:22:46 +0530

    Refactor skill component to use image icons instead of React icons. Adjust layout and styling for improved responsiveness and visual consistency.

--- src/components/skill.js
+++ src/components/skill.js
@@ -1,28 +1,26 @@
 import classes from '../styles/skill.module.scss'
-import {
-    SiReact, SiNextdotjs, SiMongodb, SiExpress, SiNodedotjs, SiGit, SiGithub, SiAmazonaws,
-    SiHtml5, SiCss3, SiJavascript, SiBootstrap, SiMui, SiRedis, SiDocker, SiRedux, SiPostman, SiCplusplus
-} from "react-icons/si"
+
+const DEVICON_BASE = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons"
 
 const skillInfo = [
-    { name: "React", Icon: SiReact, color: "#61DAFB", key: 0 },
-    { name: "Next.js", Icon: SiNextdotjs, color: "#FFFFFF", key: 1 },
-    { name: "MongoDB", Icon: SiMongodb, color: "#47A248", key: 2 },
-    { name: "Express", Icon: SiExpress, color: "#FFFFFF", key: 3 },
-    { name: "Node.js", Icon: SiNodedotjs, color: "#5FA04E", key: 4 },
-    { name: "Git", Icon: SiGit, color: "#F05032", key: 5 },
-    { name: "GitHub", Icon: SiGithub, color: "#F0F6FC", key: 6 },
-    { name: "AWS", Icon: SiAmazonaws, color: "#FF9900", key: 7 },
-    { name: "HTML", Icon: SiHtml5, color: "#E34F26", key: 8 },
-    { name: "CSS", Icon: SiCss3, color: "#1572B6", key: 9 },
-    { name: "JavaScript", Icon: SiJavascript, color: "#F7DF1E", key: 10 },
-    { name: "Bootstrap", Icon: SiBootstrap, color: "#7952B3", key: 11 },
-    { name: "MUI", Icon: SiMui, color: "#007FFF", key: 12 },
-    { name: "Redis", Icon: SiRedis, color: "#DC382D", key: 13 },
-    { name: "Docker", Icon: SiDocker, color: "#2496ED", key: 14 },
-    { name: "Redux", Icon: SiRedux, color: "#764ABC", key: 15 },
-    { name: "Postman", Icon: SiPostman, color: "#FF6C37", key: 16 },
-    { name: "C++", Icon: SiCplusplus, color: "#00599C", key: 17 },
+    { name: "React", slug: "react/react-original", key: 0 },
+    { name: "Next.js", slug: "nextjs/nextjs-original", key: 1 },
+    { name: "MongoDB", slug: "mongodb/mongodb-original", key: 2 },
+    { name: "Express", slug: "express/express-original", invert: true, key: 3 },
+    { name: "Node.js", slug: "nodejs/nodejs-original", key: 4 },
+    { name: "Git", slug: "git/git-original", key: 5 },
+    { name: "GitHub", slug: "github/github-original", invert: true, key: 6 },
+    { name: "AWS", slug: "amazonwebservices/amazonwebservices-original-wordmark", key: 7 },
+    { name: "HTML5", slug: "html5/html5-original", key: 8 },
+    { name: "CSS3", slug: "css3/css3-original", key: 9 },
+    { name: "JavaScript", slug: "javascript/javascript-original", key: 10 },
+    { name: "Bootstrap", slug: "bootstrap/bootstrap-original", key: 11 },
+    { name: "MUI", slug: "materialui/materialui-original", key: 12 },
+    { name: "Redis", slug: "redis/redis-original", key: 13 },
+    { name: "Docker", slug: "docker/docker-original", key: 14 },
+    { name: "Redux", slug: "redux/redux-original", key: 15 },
+    { name: "Postman", slug: "postman/postman-original", key: 16 },
+    { name: "C++", slug: "cplusplus/cplusplus-original", key: 17 },
 ]
 
 export default function Skills(){
@@ -33,15 +31,18 @@ export default function Skills(){
                 <span className={classes.titleLine}></span>
             </p>
             <div className={classes.skillGrid} data-aos="fade-up">
-                {skillInfo.map((skill) => {
-                    const Icon = skill.Icon
-                    return (
-                        <div key={skill.key} className={classes.skillCard} style={{ '--skill-color': skill.color }}>
-                            <Icon className={classes.skillIcon} />
-                            <p className={classes.skillName}>{skill.name}</p>
-                        </div>
-                    )
-                })}
+                {skillInfo.map((skill) => (
+                    <div key={skill.key} className={classes.skillCard}>
+                        <span className={classes.skillIcon}>
+                            <img
+                                src={`${DEVICON_BASE}/${skill.slug}.svg`}
+                                alt={skill.name}
+                                className={skill.invert ? classes.iconInvert : undefined}
+                            />
+                        </span>
+                        <p className={classes.skillName}>{skill.name}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
