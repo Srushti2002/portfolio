3899bf1cf3d327085b7605fc1e89aec7529fa6b6
Srushti2002 <srushtideshmukh54@gmail.com>
2026-09-18 23:09:40 +0530

    Update navbar components and styles; add "About" link, enhance layout, and improve responsiveness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,6 +18,7 @@ export default function Navbar(){
             }
             {show &&
                 <ul onClick={toggle} className={classes.mobileNavList}>
+                    <li><a href="#newHead">About</a></li>
                     <li><a href="#newExperience">Experience</a></li>
                     <li><a href="#newProject">Projects</a></li>
                     <li><a href="#newSkill">Skills</a></li>
@@ -25,10 +26,6 @@ export default function Navbar(){
                     <li><a href="#newContact">Contact</a></li>
                     <li className={classes.mobileNavDivider}></li>
                     <li><a href="/Srushti_Deshmukh_CV.pdf" target="_blank">Resume</a></li>
-                    <li><a href="https://www.linkedin.com/in/srushti-deshmukh-616111221/">LinkedIn</a></li>
-                    <li><a href="https://github.com/Srushti2002">Github</a></li>
-                    <li><a href="https://instagram.com/srushtideshmukh27?igshid=ZWIzMWE5ZmU3Zg==">Instagram</a></li>
-                    <li><a href="https://mail.google.com/mail/?view=cm&to=srushtideshmukh54@gmail.com">Email</a></li>
                 </ul>
             }
         </div>
--- src/components/pcnavbar.js
+++ src/components/pcnavbar.js
@@ -5,19 +5,23 @@ import Navbar from './navbar'
 export default function Pcnavbar(){
     return(
         <nav className={classes.navBar}>
-            <div className={classes.navBarRoot}>
-                <a href="#newHead" className={classes.navLogo}>SD</a>
+            <div className={classes.navPill}>
+                <a href="#newHead" className={classes.navLogo}>Srushti Deshmukh</a>
 
                 <ul className={classes.navBarList}>
-                    <li><a href="#newExperience"><span>01.</span> Experience</a></li>
-                    <li><a href="#newProject"><span>02.</span> Projects</a></li>
-                    <li><a href="#newSkill"><span>03.</span> Skills</a></li>
-                    <li><a href="#newEducation"><span>04.</span> Education</a></li>
-                    <li><a href="#newContact"><span>05.</span> Contact</a></li>
+                    <li><a href="#newHead">About</a></li>
+                    <li><a href="#newExperience">Experience</a></li>
+                    <li><a href="#newProject">Projects</a></li>
+                    <li><a href="#newSkill">Skills</a></li>
+                    <li><a href="#newEducation">Education</a></li>
+                    <li><a href="#newContact">Contact</a></li>
                 </ul>
 
-                <div className={classes.navMobile}>
-                    <Navbar />
+                <div className={classes.navActions}>
+                    <a href="/Srushti_Deshmukh_CV.pdf" target="_blank" className={classes.navResume}>Resume</a>
+                    <div className={classes.navMobile}>
+                        <Navbar />
+                    </div>
                 </div>
             </div>
         </nav>
